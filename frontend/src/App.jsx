0cf309a2cead54ebd05f03abe48d58aa0a74f9cb
Shehan Sanjula <116501697+ShehanSanjula123@users.noreply.github.com>
2024-12-30 22:08:01 +0530

    start the project with frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Home from './Pages/Home/Home';
+
+import './App.css'
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+        <Toaster/>
+      </Router>
+    </>
+  )
+}
+
+export default App
